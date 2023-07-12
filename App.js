864435fc97ce9497e4b0f71fb2edfc15eaf7f847
Jake Liu <123107892+JakeLiu-web@users.noreply.github.com>
2023-07-12 04:11:24 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import StackDemo from './components/StackDemo';
-import SummaryScreen from './components/SummaryScreen';
+import React from 'react';
+import HomePage from './components/HomePage';
+import LocationPage from './components/LocationPage';
+import WeatherPage from './components/WeatherPage';
+import GalleryPage from './components/GalleryPage';
+import PhonePage from './components/PhonePage';
 
-const App = () => {
-  const [showSummary, setShowSummary] = useState(false);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 'Home',
+    };
+  }
 
-  const handleShowSummary = () => {
-    setShowSummary(true);
+  navigateTo = (pageName) => {
+    this.setState({ currentPage: pageName });
   };
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Text style={{ textAlign: 'center', fontSize: 40 }}>StackDemo</Text>
-      {/* Render the StackDemo component */}
-      <StackDemo />
-      {/* Conditionally render the SummaryScreen component */}
-      {showSummary && <SummaryScreen />}
-      {/* Button to trigger showing the SummaryScreen */}
-      <Button title="Show Summary" onPress={handleShowSummary} />
-    </View>
-  );
-};
+  renderPage = () => {
+    const { currentPage } = this.state;
+
+    switch (currentPage) {
+      case 'Home':
+        return <HomePage navigateTo={this.navigateTo} />;
+      case 'Location':
+        return <LocationPage navigateTo={this.navigateTo} />;
+      case 'Weather':
+        return <WeatherPage navigateTo={this.navigateTo} />;
+      case 'Gallery':
+        return <GalleryPage navigateTo={this.navigateTo} />;
+      case 'Phone':
+        return <PhonePage navigateTo={this.navigateTo} />;
+      default:
+        return null;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
 
 export default App;
